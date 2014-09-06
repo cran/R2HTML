3710e722ba5e0a7a839a6f19dcb1f81a7283463c
Milan Bouchet-Valat <nalimilan@club.fr>
2014-09-06 00:00:00 +0000

    version 2.3.0

--- R/grid.R
+++ R/grid.R
@@ -78,7 +78,7 @@
 
 "HTMLgrid" <- function(
     x, 
-    file = get(".HTML.file"), 
+    file = HTMLGetFile(), 
     append=TRUE ,
     includeref=FALSE,
     align="center", 
@@ -147,7 +147,7 @@
 
 "HTMLgrid_inline" <- function(
    x,
-   file = get(".HTML.file"),
+   file = HTMLGetFile(),
    append=TRUE ,
    includeref=FALSE,
    align="center",
